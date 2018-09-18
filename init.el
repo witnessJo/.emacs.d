4a9ae6ea62bd3a477ab86c37bb8388c5b6844839
jongyoungcha <jongyoungcha@github.com>
2018-09-18 21:49:51 +0900

    ...

--- init.el
+++ init.el
@@ -435,18 +435,8 @@ With argument ARG, do this that many times."
   (setq company-async-timeout 4)
   (setq company-dabbrev-downcase nil)
   (setq company-idle-delay 0.1)
-  (add-hook 'after-init-hook 'global-company-mode)
-  ;; (defun piotr/company-complete-and-self-inser ()
-  ;;   "Complete the current selection and self-insert."
-  ;;   (interactive)
-  ;;   (company-complete-selection)
-  ;;   (call-interactively 'self-insert-command))
-
-  ;; (define-key company-active-map
-  ;;   (kbd ".") 'piotr/company-complete-and-self-inser)
-  ;; (define-key company-active-map
-  ;;   (kbd "SPC")' piotr/company-complete-and-self-inser)
-  )
+  (setq company-minimum-prefix-length 1)
+  (add-hook 'after-init-hook 'global-company-mode))
 
 
 (use-package magit
@@ -694,7 +684,7 @@ With argument ARG, do this that many times."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (indium flymake-json nodejs-repl js-commint sbt-mode ensime function-args functions-args autopair cargo emacs-racer rust-mode markdown-mode xterm-color use-package solarized-theme smart-compile register-list psvn multi-term magit hungry-delete helm-projectile helm-ag flycheck exec-path-from-shell evil elpy company-rtags company-jedi color-theme-sanityinc-tomorrow cmake-ide auto-package-update auto-highlight-symbol anaconda-mode)))
+    (js2-refactor indium flymake-json nodejs-repl js-commint sbt-mode ensime function-args functions-args autopair cargo emacs-racer rust-mode markdown-mode xterm-color use-package solarized-theme smart-compile register-list psvn multi-term magit hungry-delete helm-projectile helm-ag flycheck exec-path-from-shell evil elpy company-rtags company-jedi color-theme-sanityinc-tomorrow cmake-ide auto-package-update auto-highlight-symbol anaconda-mode)))
  '(safe-local-variable-values
    (quote
     ((projectile-project-compilation-cmd . "cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .; make")
--- jongyoungcha/jong-nodejs.el
+++ jongyoungcha/jong-nodejs.el
@@ -1,14 +1,17 @@
-
 (use-package js2-mode
   :ensure t)
+
 (use-package js2-refactor
   :ensure t)
+
 (use-package xref-js2
   :ensure t)
-;; (use-package indium
+
+;; (use-package nodejs-repl
 ;;   :ensure t)
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(use-package indium
+  :ensure t)
 
 (use-package tern
   :ensure t)
@@ -19,37 +22,35 @@
      (define-key tern-mode-keymap (kbd "C-c C-b") nil)
      (define-key tern-mode-keymap (kbd "C-c C-r") nil)))
 
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-(use-package nodejs-repl
-  :ensure t
-  :bind())
 
 (use-package company-tern
   :ensure t)
-(add-to-list 'company-backend 'company-tern)
+(add-to-list 'company-backends 'company-tern)
 
-(add-hook 'js2-mode-hook #'tern-mode)
+(add-hook 'js2-mode-hook #'tern)
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
+(add-hook 'js2-mode-hook #'indium-interaction-mode)
 
 (add-hook 'js2-mode-hook (lambda ()
                            (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
 (add-hook 'js2-mode-hook (lambda ()
                            (tern-mode)
                            (company-mode)))
+
 (add-hook 'js2-mode-hook (lambda ()
-                           ;; (local-set-key (kbd "M-.") nil)
-                           ;; (local-set-key (kbd "M-,") nil)
+
                            (local-set-key (kbd "C-c r .") 'xref-find-definitions)
                            (local-set-key (kbd "C-c r ,") 'xref-find-references)
                            (define-key js2-mode-map (kbd "C-c r .") 'xref-find-definitions)
                            (define-key js2-mode-map (kbd "C-c r ,") 'xref-find-references)
-                           (define-key js2-mode-map (kbd "C-c C-e") 'nodejs-repl-send-last-expression)
-                           (define-key js2-mode-map (kbd "C-c C-j") 'nodejs-repl-send-line)
-                           (define-key js2-mode-map (kbd "C-c C-c") 'nodejs-repl-send-buffer)
-                           (define-key js2-mode-map (kbd "C-c C-r") 'nodejs-repl-send-region)
-                           (define-key js2-mode-map (kbd "C-c C-l") 'nodejs-repl-load-file)
-                           (define-key js2-mode-map (kbd "C-c C-z") 'nodejs-repl-switch-to-repl)))
+                           (define-key js2-mode-map (kbd "C-c r l") 'indium-eval-last-node)
+                           (define-key js2-mode-map (kbd "C-c r r") 'indium-eval-region)
+                           (define-key js2-mode-map (kbd "C-c r b") 'indium-eval-buffer)
+                           (define-key js2-mode-map (kbd "C-c :") 'indium-inspect-expression)))
 
 
 (provide 'jong-nodejs)
