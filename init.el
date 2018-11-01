d865873dbbd52362a59470e9387978d803eafd6c
root <jongyoungcha@gmail.com>
2018-11-01 11:46:08 +0900

    ...

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/")
-	          '("marmalade" . "https://marmalade-repo.org/packages/"))
+	     '("marmalade" . "https://marmalade-repo.org/packages/"))
 
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
@@ -53,8 +53,8 @@
   (interactive)
   (if (use-region-p)
       (let (pos1 pos2)
-	     (setq pos1 (region-beginning) pos2 (region-end))
-	     (- pos2 pos1))
+	(setq pos1 (region-beginning) pos2 (region-end))
+	(- pos2 pos1))
     -1
     )
   )
@@ -66,7 +66,7 @@
   (let (length)
     (setq length (my-get-selection-length))
     (if (equal length -1)
-	     (message "regions is not activated...")
+	(message "regions is not activated...")
       (message "length : %d" length)
       )
     )
@@ -370,6 +370,7 @@ With argument ARG, do this that many times."
 (setq mark-ring-max 8)
 (setq global-mark-ring-max 8)
 (setq set-mark-command-repeat-pop t)
+(global-set-key (kbd "S-SPC") 'toggle-korean-input-method)
 (global-set-key (kbd "C-k") (lambda () (interactive)
                               (call-interactively 'comint-kill-whole-line)
                               (call-interactively 'indent-for-tab-command)))
@@ -415,12 +416,19 @@ With argument ARG, do this that many times."
 (global-set-key (kbd "C-M-i") (lambda() (interactive) (scroll-other-window 15)))
 (global-set-key (kbd "C-M-o") (lambda() (interactive) (scroll-other-window -15)))
 (global-set-key (kbd "C-x w b") 'switch-to-buffer-other-window)
-(global-set-key (kbd "C-c <") 'eyebrowse-prev-window-config)
-(global-set-key (kbd "C-c >") 'eyebrowse-next-window-config)
+
+(global-set-key (kbd "C-c <") (lambda() (interactive)
+				(call-interactively 'eyebrowse-prev-window-config)
+				(message "slot : %s" (eyebrowse--get 'current-slot))))
+
+(global-set-key (kbd "C-c >") (lambda() (interactive)
+				(call-interactively 'eyebrowse-next-window-config)
+				(message "slot : %s" (eyebrowse--get 'current-slot))))
+
 (global-set-key (kbd "C-c w w") (lambda() (interactive)
-                                  (call-interactively 'eyebrowse-switch-to-window-config-1)
-                                  (call-interactively 'eyebrowse-switch-to-window-config-2)
-                                  (call-interactively 'eyebrowse-switch-to-window-config-3)))
+				  (call-interactively 'eyebrowse-switch-to-window-config-1)
+				  (call-interactively 'eyebrowse-switch-to-window-config-2)
+				  (call-interactively 'eyebrowse-switch-to-window-config-3)))
 
 
 (defun my-reload-dir-locals-for-current-buffer ()
@@ -435,8 +443,8 @@ With argument ARG, do this that many times."
   (let ((dir default-directory))
     (dolist (buffer (buffer-list))
       (with-current-buffer buffer
-	     (when (equal default-directory dir))
-	     (my-reload-dir-locals-for-current-buffer)))))
+	(when (equal default-directory dir))
+	(my-reload-dir-locals-for-current-buffer)))))
 
 
 ;; default setting.
@@ -447,12 +455,12 @@ With argument ARG, do this that many times."
     (set-frame-parameter
      nil 'alpha
      (if (eql (cond ((numberp alpha) alpha)
-		              ((numberp (cdr alpha)) (cdr alpha))
-		              ;; Also handle undocumented (<active> <inactive>) form.
+		    ((numberp (cdr alpha)) (cdr alpha))
+		    ;; Also handle undocumented (<active> <inactive>) form.
 
-		              ((numberp (cadr alpha)) (cadr alpha)))
-	           100)
-	      '(85 . 50) '(100 . 100)))))
+		    ((numberp (cadr alpha)) (cadr alpha)))
+	      100)
+	 '(85 . 50) '(100 . 100)))))
 
 (global-set-key (kbd "C-c t") 'toggle-transparency)
 
@@ -477,9 +485,9 @@ With argument ARG, do this that many times."
   (set-face-background 'show-paren-match-face (face-background 'default))
   (if (boundp 'font-lock-comment-face)
       (set-face-foreground 'show-paren-match-face
-			                  (face-foreground 'font-lock-comment-face))
+			   (face-foreground 'font-lock-comment-face))
     (set-face-foreground 'show-paren-match-face
-			                (face-foreground 'default)))
+			 (face-foreground 'default)))
   (set-face-attribute 'show-paren-match-face nil :weight 'extra-bold))
 
 (require 'paren)
@@ -589,8 +597,8 @@ With argument ARG, do this that many times."
 ;;;; elisp develope environments ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (add-hook 'emacs-lisp-mode-hook
-	       (lambda()
-	         (local-set-key (kbd "C-c g g") 'xref-find-definitions)))
+	  (lambda()
+	    (local-set-key (kbd "C-c g g") 'xref-find-definitions)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;  python develope environments  ;;;;
@@ -611,8 +619,8 @@ With argument ARG, do this that many times."
 (use-package company-jedi
   :ensure t)
 (add-hook 'python-mode-hook
-	       (lambda()
-	         (add-to-list 'company-backend 'company-jedi)))
+	  (lambda()
+	    (add-to-list 'company-backend 'company-jedi)))
 
 (global-set-key (kbd "C-c i") 'indent-region)
 
@@ -647,36 +655,36 @@ With argument ARG, do this that many times."
 
 (defvar jo-kill-target-buffers)
 (setq jo-kill-target-buffers (list "*RTags*" "*compilation*" "*Occur*" "*Help*"
-                                   "*Warnings*" "*xref*" "*Node Shell*" "*Google Translate*"))
+				   "*Warnings*" "*xref*" "*Node Shell*" "*Google Translate*"))
 (defun kill-temporary-buffers ()
   "Kill current buffer unconditionally."
   (interactive)
   (let ((buffer-modifinnnned-p nil)
-        (buffer-to-kill nil))
+	(buffer-to-kill nil))
     (dolist (buffer-name jo-kill-target-buffers)
       (when (setq buffer-to-kill (get-buffer buffer-name))
-        (kill-buffer buffer-to-kill)))
+	(kill-buffer buffer-to-kill)))
     (if (not (equal projectile-project-name nil))
-	     (when (get-buffer (setq buffer-to-kill (format "%s-%s" "*compilation*" projectile-project-name)))
-	       (kill-buffer buffer-to-kill)))
+	(when (get-buffer (setq buffer-to-kill (format "%s-%s" "*compilation*" projectile-project-name)))
+	  (kill-buffer buffer-to-kill)))
     (delete-above-below-window))
   )
 
 
 (global-set-key (kbd "C-g")
-		          (lambda () (interactive)
-		            (kill-temporary-buffers)
-		            (keyboard-quit))
-		          )
+		(lambda () (interactive)
+		  (kill-temporary-buffers)
+		  (keyboard-quit))
+		)
 
 (add-hook 'python-mode-hook
-	       (lambda ()
-	         (local-set-key (kbd "C-c g g") 'anaconda-mode-find-definitions)
-	         (local-set-key (kbd "C-c c c") 'jyc-run-python)
-	         (local-set-key (kbd "C-g") 'kill-temporary-buffers)
-	         (local-set-key (kbd "C-S-g") 'close-compilation-window)
-	         (linum-mode t)
-	         ))
+	  (lambda ()
+	    (local-set-key (kbd "C-c g g") 'anaconda-mode-find-definitions)
+	    (local-set-key (kbd "C-c c c") 'jyc-run-python)
+	    (local-set-key (kbd "C-g") 'kill-temporary-buffers)
+	    (local-set-key (kbd "C-S-g") 'close-compilation-window)
+	    (linum-mode t)
+	    ))
 
 
 
@@ -695,13 +703,13 @@ With argument ARG, do this that many times."
    If buffer is modified, ask about save before running etags."
   (let ((extension (file-name-extension (buffer-file-name))))
     (condition-case err
-	     ad-do-it
+	ad-do-it
       (error (and (buffer-modified-p)
-		            (not (ding))
-		            (y-or-n-p "Buffer is modified, save it? ")
-		            (save-buffer))
-	          (er-refresh-etags extension)
-	          ad-do-it))))
+		  (not (ding))
+		  (y-or-n-p "Buffer is modified, save it? ")
+		  (save-buffer))
+	     (er-refresh-etags extension)
+	     ad-do-it))))
 
 (defun er-refresh-etags (&optional extension)
   "Run etags on all peer files in current dir and reload them silently."
@@ -735,8 +743,8 @@ With argument ARG, do this that many times."
 (use-package markdown-mode
   :ensure t  :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
-	      ("\\.md\\'" . markdown-mode)
-	      ("\\.markdown\\'" . markdown-mode))
+	 ("\\.md\\'" . markdown-mode)
+	 ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
 (add-to-list 'load-path "~/.emacs.d/jongyoungcha")
