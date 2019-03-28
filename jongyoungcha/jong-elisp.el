fd8775535f9673718e8e784be89efff9b2baceb4
ycjo <ycjo@bitbucket.org>
2019-03-28 14:16:06 +0900

    .................

--- jongyoungcha/jong-elisp.el
+++ jongyoungcha/jong-elisp.el
@@ -1,13 +1,48 @@
+;;; Code
+
+(use-package dash
+  :ensure t
+  :config
+  (dash-enable-font-lock))
+
+(use-package helm-dash
+  :ensure t
+  :config)
+
+(use-package dash-functional
+  :ensure t
+  :config)
+
+
+(use-package elisp-slime-nav
+  :ensure t
+  :config
+  (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+	(add-hook hook 'elisp-slime-nav-mode))
+  )
 
 (use-package elisp-refs
-  :ensure t)
+  :ensure t
+  :config)
 
-(add-hook 'emacs-lisp-mode-hook
-          (lambda ()
-            (define-key emacs-lisp-mode-map (kbd "C-c r .") 'xref-find-definitions)
-            (define-key emacs-lisp-mode-map (kbd "C-c r ,") 'elisp-refs-symbol)
-            (define-key emacs-lisp-mode-map (kbd "C-M-i") (lambda() (interactive) (scroll-other-window 15)))
-            (define-key emacs-lisp-mode-map (kbd "C-M-o") (lambda() (interactive) (scroll-other-window -15)))))
+
+(use-package log4e
+  :ensure t
+  :config
+  (log4e:deflogger "hoge" "%t [%l] %m" "%H:%M:%S" '((fatal . "fatal")
+													(error . "error")
+													(warn  . "warn")
+													(info  . "info")
+													(debug . "debug")
+													(trace . "trace"))))
+
+
+(define-key emacs-lisp-mode-map (kbd "C-c r .") 'elisp-slime-nav-find-elisp-thing-at-point)
+(define-key emacs-lisp-mode-map (kbd "C-c r ,") 'elisp-refs-symbol)
+(define-key emacs-lisp-mode-map (kbd "C-c C-u") 'xref-pop-marker-stack)
+(define-key emacs-lisp-mode-map (kbd "C-M-i") (lambda() (interactive) (scroll-other-window 15)))
+(define-key emacs-lisp-mode-map (kbd "C-M-o") (lambda() (interactive) (scroll-other-window -15)))
+
 
 
 
