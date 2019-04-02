c8bf8579d7e458c6825e60965b46ebdef97c03bd
jongyoungcha <jongyoungcha@gmail.com>
2019-04-02 22:30:31 +0900

    ...........................

--- jongyoungcha/jong-common.el
+++ jongyoungcha/jong-common.el
@@ -1,6 +1,26 @@
 ;;; Code
 
 
+(defun jong-common-find-file-other-window()
+  (interactive)
+  (let ((buffer-source (current-buffer))
+        (buffer-target))
+    (call-interactively 'helm-find-files)
+    (setq buffer-target (current-buffer))
+    (unless (eq buffer-source buffer-target)
+      (progn
+        (pop-to-buffer buffer-target 'other-window)
+        (other-window -1)
+        (pop-to-buffer-same-window buffer-source)
+        )))
+  )
+
+
+
+
+(global-set-key (kbd "C-c C-f") 'jong-common-find-file-other-window)
+ 
+
 
 (provide 'jong-common)
 
