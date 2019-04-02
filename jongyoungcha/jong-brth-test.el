f8ee92aa5953d910af1f1e5418d77f53d216bc52
jongyoungcha <jongyoungcha@gmail.com>
2019-04-02 22:50:09 +0900

    .....................

--- jongyoungcha/jong-brth-test.el
+++ jongyoungcha/jong-brth-test.el
@@ -14,34 +14,34 @@
   :type 'string)
 
 (defcustom jong-brth-node-list (list "192.168.55.100"
-									 "192.168.55.101"
-									 "192.168.55.102"
-									 "192.168.55.103"
-									 "192.168.0.160"
-									 "192.168.0.161"
-									 "192.168.0.162"
-									 "34.237.211.223" ;;AWS
-									 "3.210.9.228"    ;;AWS
-									 )
+									          "192.168.55.101"
+									          "192.168.55.102"
+									          "192.168.55.103"
+									          "192.168.0.160"
+									          "192.168.0.161"
+									          "192.168.0.162"
+									          "34.237.211.223" ;;AWS
+									          "3.210.9.228"    ;;AWS
+									          )
   "Berith node list."
   :type 'list)
 
 
 (defcustom jong-brth-node-commands
   (list "eth"
-		"miner.start(1)"
-		"miner.stop()"
-		"miner.setEtherbase(eth.accounts[0])"
-		"personal.newAccount(\"berith\")"
-		"personal.unlockAccount(eth.accounts[0], \"berith\", 0)"
-		"personal.lockAccount(eth.accounts[0], \"berith\", 0)"
-		"web3.fromWei(berith.getStakeBalance(eth.accounts[0]), \"ether\")"
-		"web3.fromWei(eth.getBalance(eth.coinbase), \"ether\")"
-		"web3.fromWei(berith.getRewardBalance(eth.coinbase), \"ether\")"
-		"berith.stake({from:eth.coinbase, value:1000000000000000000, staking:true})"
-		"berith.stopStaking(eth.coinbase)"
-		"eth.sendTransaction({from:eth.accounts[0], to:\"0x496732e14c615792dd9dc09404387938e3a8a407\", value:200000000000000000000})"
-		"admin.addPeer(\"\")")
+		  "miner.start(1)"
+		  "miner.stop()"
+		  "miner.setEtherbase(eth.accounts[0])"
+		  "personal.newAccount(\"berith\")"
+		  "personal.unlockAccount(eth.accounts[0], \"berith\", 0)"
+		  "personal.lockAccount(eth.accounts[0], \"berith\", 0)"
+		  "web3.fromWei(berith.getStakeBalance(eth.accounts[0]), \"ether\")"
+		  "web3.fromWei(eth.getBalance(eth.coinbase), \"ether\")"
+		  "web3.fromWei(berith.getRewardBalance(eth.coinbase), \"ether\")"
+		  "berith.stake({from:eth.coinbase, value:1000000000000000000, staking:true})"
+		  "berith.stopStaking(eth.coinbase)"
+		  "eth.sendTransaction({from:eth.accounts[0], to:\"0x496732e14c615792dd9dc09404387938e3a8a407\", value:200000000000000000000})"
+		  "admin.addPeer(\"\")")
   "Berith command list."
   :type 'list)
 
@@ -55,7 +55,7 @@
 (setq jong-brth-test-datadir (format "%s/%s" (getenv "HOME") "testdir"))
 (setq jong-brth-highlights
       '(("test\\|success\\|done\\|INFO\\|" . font-lock-function-name-face)
-		("failed\\|Failed\\|FAILED\\|warn\\|Warn\\|WARN\\|" . font-lock-warning-face)))
+		  ("failed\\|Failed\\|FAILED\\|warn\\|Warn\\|WARN\\|" . font-lock-warning-face)))
 
 
 (define-derived-mode jong-brth-attach-mode eshell-mode "BRTH-ATTCH"
@@ -75,29 +75,29 @@
 (defun jong-brth-show-node-log (node-host)
   "A."
   (let ((node-host-buffer-name (jong-brth-get-log-buffer-name node-host))
-		(node-host-buffer))
+		  (node-host-buffer))
     (if (setq node-host-buffer (get-buffer node-host-buffer-name))
-		(if (get-buffer-process node-host-buffer)
-			(progn
-			  (switch-to-buffer node-host-buffer)
-			  (goto-char (point-min))
-			  (goto-char (point-max)))
-		  (with-current-buffer (get-buffer-create node-host-buffer-name)
-			(setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
-			(ignore-errors (async-shell-command "tail -f ./geth.log" (current-buffer) (current-buffer)))
-			(other-window 1)
-			(sleep-for 1)
-			(goto-char (point-min))
-			(goto-char (point-max))
-			))
+		  (if (get-buffer-process node-host-buffer)
+			   (progn
+			     (switch-to-buffer node-host-buffer)
+			     (goto-char (point-min))
+			     (goto-char (point-max)))
+		    (with-current-buffer (get-buffer-create node-host-buffer-name)
+			   (setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
+			   (ignore-errors (async-shell-command "tail -f ./geth.log" (current-buffer) (current-buffer)))
+			   (other-window 1)
+			   (sleep-for 1)
+			   (goto-char (point-min))
+			   (goto-char (point-max))
+			   ))
       (with-current-buffer (get-buffer-create node-host-buffer-name)
-		(setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
-		(ignore-errors (async-shell-command "tail -f ./geth.log" (current-buffer) (current-buffer)))
-		(other-window 1)
-		
-		(sleep-for 1)
-		(goto-char (point-min))
-		(goto-char (point-max)))
+		  (setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
+		  (ignore-errors (async-shell-command "tail -f ./geth.log" (current-buffer) (current-buffer)))
+		  (other-window 1)
+		  
+		  (sleep-for 1)
+		  (goto-char (point-min))
+		  (goto-char (point-max)))
       ))
   )
 
@@ -105,11 +105,11 @@
   (interactive)
   (let ((selected-node nil))
     (setq selected-node  (helm :sources (helm-build-sync-source "Berith nodes for log."
-										  :candidates jong-brth-node-list
-										  :fuzzy-match t
-										  :action (lambda (node)
-													node))
-							   :buffer "*jong-berith-nodes-log*"))
+										            :candidates jong-brth-node-list
+										            :fuzzy-match t
+										            :action (lambda (node)
+													             node))
+							          :buffer "*jong-berith-nodes-log*"))
     (when selected-node (jong-brth-show-node-log selected-node)))
   )
 
@@ -120,41 +120,41 @@
 (defun jong-brth-show-node-attach (node-host)
   "A."
   (let ((node-host-buffer-name (jong-brth-get-attach-buffer-name node-host))
-		(node-host-buffer))
+		  (node-host-buffer))
     (if (setq node-host-buffer (get-buffer node-host-buffer-name))
-		(if (get-buffer-process node-host-buffer)
-			(progn
-			  (switch-to-buffer node-host-buffer)
-			  (goto-char (point-min))
-			  (goto-char (point-max)))
-		  (with-current-buffer (get-buffer-create node-host-buffer-name)
-			(setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
-			(ignore-errors (shell (current-buffer)))
-			(jong-brth-exec-command "source ./.bash_profile\n")
-			;; (jong-brth-exec-command "brth-kill")
-			;; (sleep-for 1)
-			;; (jong-brth-exec-command "brth-init-poa")
-			;; (sleep-for 1)
-			;; (jong-brth-exec-command "brth-run")
-			;; (sleep-for 1)
-			(jong-brth-exec-command "brth-attach\n")
-			(goto-char (point-min))
-			(goto-char (point-max))
-			))
+		  (if (get-buffer-process node-host-buffer)
+			   (progn
+			     (switch-to-buffer node-host-buffer)
+			     (goto-char (point-min))
+			     (goto-char (point-max)))
+		    (with-current-buffer (get-buffer-create node-host-buffer-name)
+			   (setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
+			   (ignore-errors (shell (current-buffer)))
+			   (jong-brth-exec-command "source ./.bash_profile\n")
+			   ;; (jong-brth-exec-command "brth-kill")
+			   ;; (sleep-for 1)
+			   ;; (jong-brth-exec-command "brth-init-poa")
+			   ;; (sleep-for 1)
+			   ;; (jong-brth-exec-command "brth-run")
+			   ;; (sleep-for 1)
+			   (jong-brth-exec-command "brth-attach\n")
+			   (goto-char (point-min))
+			   (goto-char (point-max))
+			   ))
       (with-current-buffer (get-buffer-create node-host-buffer-name)
-		(setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
-		(ignore-errors (shell (current-buffer)))
-		(jong-brth-exec-command "source ./.bash_profile\n")
-		;; (jong-brth-exec-command "brth-kill")
-		;; (sleep-for 1)
-		;; (jong-brth-exec-command "brth-init-poa")
-		;; (sleep-for 1)
-		;; (jong-brth-exec-command "brth-run")
-		;; (sleep-for 1)
-		(jong-brth-exec-command "brth-attach\n")
-		(goto-char (point-min))
-		(goto-char (point-max))
-		)
+		  (setq default-directory (format "/ssh:%s@%s:" jong-brth-user node-host))
+		  (ignore-errors (shell (current-buffer)))
+		  (jong-brth-exec-command "source ./.bash_profile\n")
+		  ;; (jong-brth-exec-command "brth-kill")
+		  ;; (sleep-for 1)
+		  ;; (jong-brth-exec-command "brth-init-poa")
+		  ;; (sleep-for 1)
+		  ;; (jong-brth-exec-command "brth-run")
+		  ;; (sleep-for 1)
+		  (jong-brth-exec-command "brth-attach\n")
+		  (goto-char (point-min))
+		  (goto-char (point-max))
+		  )
       ))
   )
 
@@ -163,11 +163,11 @@
   (interactive)
   (let ((brth-command nil))
     (setq brth-command  (helm :sources (helm-build-sync-source "Berith Commands."
-										 :candidates jong-brth-node-commands
-										 :fuzzy-match t
-										 :action (lambda (node)
-												   node))
-							  :buffer "*Berith Commands*"))
+										           :candidates jong-brth-node-commands
+										           :fuzzy-match t
+										           :action (lambda (node)
+												               node))
+							         :buffer "*Berith Commands*"))
     
     (jong-brth-exec-command brth-command))
   )
@@ -176,14 +176,15 @@
 
 (defun jong-brth-select-node-attach ()
   (interactive)
-  (let ((selected-node nil))
-    (setq selected-node (helm :sources (helm-build-sync-source "Berith nodes for attach."
-										 :candidates jong-brth-node-list
-										 :fuzzy-match t
-										 :action (lambda (node)
-												   node))
-							  :buffer "*jong-berith-nodes-attach*"))
-    (when selected-node (jong-brth-show-node-attach selected-node))
+  (let ((selected-nodes nil))
+    (setq selected-nodes (helm :sources (helm-build-sync-source "Berith nodes for attach."
+										            :candidates jong-brth-node-list
+										            :fuzzy-match t
+										            :action (lambda (node)
+												                node))
+							          :buffer "*jong-berith-nodes-attach*"))
+    (dolist (brth-node selected-nodes)
+      (jong-brth-show-node-attach brth-node))
     )
   )
 
@@ -191,46 +192,46 @@
 (defun jong-brth-all-node-attach ()
   (interactive)
   (let ((value nil))
-	(dolist (elem jong-brth-node-list)
-	  (jong-brth-show-node-attach elem)))
+	 (dolist (elem jong-brth-node-list)
+	   (jong-brth-show-node-attach elem)))
   )
 
 
 (defun jong-brth-all-node-log ()
   (interactive)
   (let ((value nil))
-	(dolist (elem jong-brth-node-list)
-	  (jong-brth-show-node-log elem)))
+	 (dolist (elem jong-brth-node-list)
+	   (jong-brth-show-node-log elem)))
   )
 
 
 (defun jong-brth-get-attach-buffers ()
   (let ((target-buffers))
-	(dolist (elem (buffer-list))
-	  (when (string-match "^*brth-node-attach.*" (buffer-name elem))
-		(setq target-buffers (cons elem target-buffers))))
-	target-buffers
-	)
+	 (dolist (elem (buffer-list))
+	   (when (string-match "^*brth-node-attach.*" (buffer-name elem))
+		  (setq target-buffers (cons elem target-buffers))))
+	 target-buffers
+	 )
   )
 
 (defun jong-brth-get-node-coinbases ()
   (let ((coinbases (list))
-		(target-buffers))
-	(setq target-buffers (jong-brth-get-attach-buffers))
-	(dolist (target-buffer target-buffers)
-	  (with-current-buffer target-buffer
-		(jong-brth-exec-command "eth.coinbase")
-		(sleep-for 1)
-		;; (message "%s" (point))
-		(forward-line -1)
-		(beginning-of-line)
-		(setq start-pos (point))
-		(end-of-line)
-		(setq end-pos (point))
-		(setq coinbases (cons (substring (buffer-string) (1- start-pos) end-pos) coinbases)))
-	  )
-	coinbases
-	)
+		  (target-buffers))
+	 (setq target-buffers (jong-brth-get-attach-buffers))
+	 (dolist (target-buffer target-buffers)
+	   (with-current-buffer target-buffer
+		  (jong-brth-exec-command "eth.coinbase")
+		  (sleep-for 1)
+		  ;; (message "%s" (point))
+		  (forward-line -1)
+		  (beginning-of-line)
+		  (setq start-pos (point))
+		  (end-of-line)
+		  (setq end-pos (point))
+		  (setq coinbases (cons (substring (buffer-string) (1- start-pos) end-pos) coinbases)))
+	   )
+	 coinbases
+	 )
   )
 
 
@@ -238,29 +239,29 @@
 (defun jong-brth-all-send-berith-to-coinbase ()
   (interactive)
   (let ((coinbases (list))
-		(target-buffers (list))
-		(start-pos)
-		(end-pos))
-	(dolist (elem (buffer-list) coinbases)
-	  (when (string-match "^*brth-node-attach.*" (buffer-name elem))
-		(setq target-buffers (cons elem target-buffers)))
-	  )
-	(dolist (target-buffer target-buffers)
-	  (with-current-buffer target-buffer
-		(jong-brth-exec-command "eth.coinbase")
-		(sleep-for 1)
-		;; (message "%s" (point))
-		(forward-line -1)
-		(beginning-of-line)
-		(setq start-pos (point))
-		(end-of-line)
-		(setq end-pos (point))
-		(setq coinbases (cons (substring (buffer-string) (1- start-pos) end-pos) coinbases)))
-	  )
-	(dolist (coinbase coinbases)
-	  (jong-brth-exec-command (format "eth.sendTransaction({from:eth.accounts[0], to:%s, value:200000000000000000000})\n" coinbase))
-	  )
-	)
+		  (target-buffers (list))
+		  (start-pos)
+		  (end-pos))
+	 (dolist (elem (buffer-list) coinbases)
+	   (when (string-match "^*brth-node-attach.*" (buffer-name elem))
+		  (setq target-buffers (cons elem target-buffers)))
+	   )
+	 (dolist (target-buffer target-buffers)
+	   (with-current-buffer target-buffer
+		  (jong-brth-exec-command "eth.coinbase")
+		  (sleep-for 1)
+		  ;; (message "%s" (point))
+		  (forward-line -1)
+		  (beginning-of-line)
+		  (setq start-pos (point))
+		  (end-of-line)
+		  (setq end-pos (point))
+		  (setq coinbases (cons (substring (buffer-string) (1- start-pos) end-pos) coinbases)))
+	   )
+	 (dolist (coinbase coinbases)
+	   (jong-brth-exec-command (format "eth.sendTransaction({from:eth.accounts[0], to:%s, value:200000000000000000000})\n" coinbase))
+	   )
+	 )
   )
 
 
@@ -268,56 +269,56 @@
   "Run add peer to other nodes."
   (interactive)
   (let ((start-pos)
-		(end-pos)
-		(host-buffer-name)
-		(buffer-host)
-		(enode-full)
-		(enode-prefix)
-		(enode-host)
-		(enode-postfix)
-		(enode-completed)
-		(buffer-list)
-		(command-to-send))
-	
-	(with-current-buffer (current-buffer)
-	  ;; Get enode information.
-	  
-	  (jong-brth-exec-command "admin.nodeInfo.enode")
-	  (sleep-for 1)
-	  (goto-char (point-max))
-	  (forward-line -1)
-	  (beginning-of-line)
-	  (setq start-pos (1- (point)))
-	  (end-of-line)
-	  (setq end-pos (point))
-	  (setq enode-full (substring (buffer-string) start-pos end-pos))
-	  (message "%s" enode-full)
+		  (end-pos)
+		  (host-buffer-name)
+		  (buffer-host)
+		  (enode-full)
+		  (enode-prefix)
+		  (enode-host)
+		  (enode-postfix)
+		  (enode-completed)
+		  (buffer-list)
+		  (command-to-send))
+	 
+	 (with-current-buffer (current-buffer)
+	   ;; Get enode information.
+	   
+	   (jong-brth-exec-command "admin.nodeInfo.enode")
+	   (sleep-for 1)
+	   (goto-char (point-max))
+	   (forward-line -1)
+	   (beginning-of-line)
+	   (setq start-pos (1- (point)))
+	   (end-of-line)
+	   (setq end-pos (point))
+	   (setq enode-full (substring (buffer-string) start-pos end-pos))
+	   (message "%s" enode-full)
 
-	  ;; Get buffer origin host
-	  (setq host-buffer-name (buffer-name))
-	  (if (string-match "\\([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\\)" host-buffer-name)
-		  (setq buffer-host (match-string 1 host-buffer-name))))
-	
-	(if (string-match "^\\(.*@\\)\\(.*\\)\\(\:.*\\)" enode-full)
-		(progn
-		  (setq enode-prefix (match-string 1 enode-full))
-		  (setq enode-host (match-string 2 enode-full))
-		  (setq enode-postfix (match-string 3 enode-full))
-		  (print enode-prefix)
-		  (print enode-host)
-		  (print enode-postfix)))
-	(setq enode-completed (format "%s%s%s" enode-prefix buffer-host enode-postfix))
-	(message "%s" enode-completed)
-	(setq command-to-send (format "admin.addPeer(%s)" enode-completed))
-	(message "%s" command-to-send)
-	
-	(setq buffer-list (jong-brth-get-attach-buffers))
-	(dolist (buffer buffer-list)
-	  (with-current-buffer buffer
-		(goto-char (point-max))
-		(ignore-errors (jong-brth-exec-command command-to-send)))
-	  )
-	)
+	   ;; Get buffer origin host
+	   (setq host-buffer-name (buffer-name))
+	   (if (string-match "\\([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\\)" host-buffer-name)
+		    (setq buffer-host (match-string 1 host-buffer-name))))
+	 
+	 (if (string-match "^\\(.*@\\)\\(.*\\)\\(\:.*\\)" enode-full)
+		  (progn
+		    (setq enode-prefix (match-string 1 enode-full))
+		    (setq enode-host (match-string 2 enode-full))
+		    (setq enode-postfix (match-string 3 enode-full))
+		    (print enode-prefix)
+		    (print enode-host)
+		    (print enode-postfix)))
+	 (setq enode-completed (format "%s%s%s" enode-prefix buffer-host enode-postfix))
+	 (message "%s" enode-completed)
+	 (setq command-to-send (format "admin.addPeer(%s)" enode-completed))
+	 (message "%s" command-to-send)
+	 
+	 (setq buffer-list (jong-brth-get-attach-buffers))
+	 (dolist (buffer buffer-list)
+	   (with-current-buffer buffer
+		  (goto-char (point-max))
+		  (ignore-errors (jong-brth-exec-command command-to-send)))
+	   )
+	 )
   )
 
 
@@ -330,22 +331,22 @@
 (defun jong-brth-send-transactions ()
   (interactive)
   (let ((period-miliseconds)
-		(target-address)
-		(times)
-		(command))
-	;; (setq target-address (read-string "The address to send :" ))
-	;; (setq period-miliseconds (string-to-number (read-string "The perios between transactions (milliseconds) : ")))
-	;; (setq times (string-to-number (read-string "How many times : ")))
-	(setq target-address "0xd3d78151950c063fedc6c9b8083e09502205c079")
-	(setq period-miliseconds 10)
-	(setq times 1000)
-	
-	(dotimes (i times)
-	  (setq command (format "eth.sendTransaction({from:eth.accounts[0], to:\"%s\", value:1000000000000000000})" target-address))
-	  (sleep-for 0 period-miliseconds)
-	  (jong-brth-exec-command command)
-	  )
-	)
+		  (target-address)
+		  (times)
+		  (command))
+	 ;; (setq target-address (read-string "The address to send :" ))
+	 ;; (setq period-miliseconds (string-to-number (read-string "The perios between transactions (milliseconds) : ")))
+	 ;; (setq times (string-to-number (read-string "How many times : ")))
+	 (setq target-address "0xd3d78151950c063fedc6c9b8083e09502205c079")
+	 (setq period-miliseconds 10)
+	 (setq times 1000)
+	 
+	 (dotimes (i times)
+	   (setq command (format "eth.sendTransaction({from:eth.accounts[0], to:\"%s\", value:1000000000000000000})" target-address))
+	   (sleep-for 0 period-miliseconds)
+	   (jong-brth-exec-command command)
+	   )
+	 )
   )
 
 
