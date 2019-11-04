9201ed77fe338664c94fd248832f81cccdbcfc54
jongyoungcha <jongyoungcha@gmail.com>
2019-11-04 03:23:58 -0500

    .........................

--- jongyoungcha/jong-project.el
+++ jongyoungcha/jong-project.el
@@ -205,7 +205,8 @@
 				(progn
 					(setq default-directory debug-default-dir)
 					(cond
-					 ((equal major-mode 'c++-mode) (gud-gdb debug-cmd))
+					 ((equal major-mode 'c++-mode)
+						(call-interactively 'gud-gdb))
 					 ((equal major-mode 'go-mode) (dlv debug-cmd))))
 			(message "\"projectile-project-root\" and \"jong-project-debug-command were not binded."))
 		)
