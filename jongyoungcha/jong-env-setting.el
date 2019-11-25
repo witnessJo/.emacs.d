7f84069af9e55bff48976dc38608467a3258ad4b
richard <richard@swit.io>
2019-11-25 18:01:18 +0900

    ...........

--- jongyoungcha/jong-env-setting.el
+++ jongyoungcha/jong-env-setting.el
@@ -1,55 +1,59 @@
 ;;; Code:
 
+(define-coding-system-alias 'UTF-8 'utf-8)
 (defcustom jong-env-locale-value "utf-8"
 	"A variable for locale language setting.")
 
 (cond
- ((string-equal system-type "windows-nt") ; Microsoft Windows
-	(progn
-		(define-key key-translation-map [C-kanji] (kbd "C-SPC"))
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-		;; Alias coding system for windows ;;
-		;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+ ((string-equal system-type "windows-nt") ; Micro©gsoft Windows
+  (progn
+    (define-key key-translation-map [C-kanji] (kbd "C-SPC"))
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    	;; Alias coding system for windows ;;
+	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    (set-language-environment "Korean")
+    (define-coding-system-alias 'cp65001 'utf-8)
+    (setq jong-env-locale-value 'utf-8)
+    (set-default-coding-systems jong-env-locale-value)
+    (set-input-method 'korean-hangul)
 
-		(set-language-environment "Korean")
-		(define-coding-system-alias 'cp65001 'utf-8)
-		(setq jong-env-locale-value 'utf-8)
-		(set-default-coding-systems jong-env-locale-value)
-		(set-input-method 'korean-hangul)
-
-		(load-theme 'sanityinc-tomorrow-blue t)
-		))
+    (load-theme 'sanityinc-tomorrow-blue t)
+    ))
 
  ((string-equal system-type "darwin") ; Mac OS X
-	(progn
-		(message "Mac OS X")))
-
- ((string-equal system-type "gnu/linux") ; linux
-	(progn
-		(set-language-environment "Korean")
-		(with-no-warnings (setq projectile-indexing-method 'hybrid))
-
+  (progn
+		(setq shell-file-name "bash")
+		(setq shell-command-switch "-ic")
+		
 		(load-theme 'sanityinc-tomorrow-blue t)
-		;; (load-theme 'solarized-dark t)
-		(setq jong-env-locale-value
-					(if (string= (getenv "LANG") "ko_KR.utf8") 'utf-8 'euc-kr))
+    (message "Mac OS X")))
+ ((string-equal system-type "gnu/linux") ; linux
+  (progn
+    
+    (set-language-environment "Korean")
+    (with-no-warnings (setq projectile-indexing-method 'hybrid))
+    
+    (setq jong-env-locale-value
+	  (if (string= (getenv "LANG") "ko_KR.utf8") 'utf-8 'euc-kr))
+    (prefer-coding-system jong-env-locale-value)
+    (set-default-coding-systems jong-env-locale-value)
+    (set-language-environment 'utf-8)
+    (set-input-method 'korean-hangul)
+    (setq-default file-name-coding-system jong-env-locale-value)
+    (setq-default locale-coding-system jong-env-locale-value)
+    (set-terminal-coding-system jong-env-locale-value)
+    (set-keyboard-coding-system jong-env-locale-value)
+    (set-selection-coding-system jong-env-locale-value)
+    (dynamic-completion-mode)
+    (load-theme 'solarized-dark t)
 
-		(message "Linux"))))
+    (message "Linux"))))
 
 (when (member "Consolas" (font-family-list))
 	(set-face-attribute 'default nil :font "Consolas-12")
 	(setq-default line-spacing 2))
 
-(prefer-coding-system jong-env-locale-value)
-(set-default-coding-systems jong-env-locale-value)
-(set-language-environment 'UTF-8)
-(set-input-method 'korean-hangul)
-(setq-default file-name-coding-system jong-env-locale-value)
-(setq-default locale-coding-system jong-env-locale-value)
-(set-terminal-coding-system jong-env-locale-value)
-(set-keyboard-coding-system jong-env-locale-value)
-(set-selection-coding-system jong-env-locale-value)
-(dynamic-completion-mode)
+
 
 (setq-default indent-tabs-mode t)
 (setq-default tab-width 4)
