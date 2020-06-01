08b91568902bc723318d4bdb0e3ffd12e1f0b50b
richard <jongyoungcha@gmail.com>
2020-06-02 00:27:12 +0900

    modify swit projects tools

--- jongyoungcha/jong-swit.el
+++ jongyoungcha/jong-swit.el
@@ -1,5 +1,18 @@
 
 
+(defgroup jong-swit nil
+  "Quickly switch current window."
+  :group 'convenience
+  :prefix "jong-swit")
+
+
+(defvar jong-swit-base (format "%s/go/src/swit" (getenv "HOME")))
+(defvar jong-swit-projects '(
+														 ("swit-grpc-task-golang" "dev.v4")
+														 ("swit-grpc-project-golang" "dev.v4")
+														 ("swit-grpc-asset-golang" "dev")
+														 ("swit-grpc-auth-golang" "dev.4")
+														 ))
 
 (defun jong-swit-set-local-envs ()
 	(interactive)
@@ -14,16 +27,45 @@
 	(setenv  "SWIT_REDIS_PASSWORD" "")
 	(setenv  "ELASTIC_SERVICE_ADDR" "10.0.0.35:50071")
 	(setenv  "WORKSPACE_SERVICE_ADDR" "10.0.0.35:50052")
-	;; (setenv  "GOOGLE_APPLICATION_CREDENTIALS" "/credentials/swit-gke-resource-access.json")
 	(setenv  "GOOGLE_APPLICATION_CREDENTIALS" "/Users/swit-mac/swit-gke-resource-access.json")
 	(setenv  "PUB_SUB_CLIENT" "swit-dev")
 	)
 
 (defun jong-swit-set-dev-envs ()
 	(interactive)
+	()
 	)
 
+(defun jong-swit-clone-base-project()
+	(interactive))
 
+
+(defun jong-swit-checkout-base-projects()
+	(interactive)
+	(let (value
+				project-name
+				branch)
+		(dolist (project jong-swit-projects nil)
+			(setq project-name (nth 0 project))
+			(setq branch (nth 1 project))
+			(with-current-buffer (get-buffer-create (format "*%s-out*" (car project)))
+				(setq default-directory (format "%s/%s" jong-swit-base project-name))
+				(shell-command (format "echo %s" project-name) (current-buffer))
+				(shell-command (format "git checkout %s" branch) (current-buffer))
+				(shell-command "git pull" (current-buffer))
+				)
+			)
+		)
+	)
+
+(defun jong-swit-run-base-projects ()
+	(interactive)
+	(let (value)
+		(dolist (elt jong-swit-projects nil)
+			(print elt)
+			)
+		)
+	)
 
 
 
