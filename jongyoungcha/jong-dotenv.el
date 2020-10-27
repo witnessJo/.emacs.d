ee4777e1233580b280e5143e8394f66f2a1256c4
richard <richard@swit.io>
2020-10-27 22:10:46 +0900

    modified jong-dotenv

--- jongyoungcha/jong-dotenv.el
+++ jongyoungcha/jong-dotenv.el
@@ -11,7 +11,29 @@
 
 
 
-
-(defun jong-dotenv-load-dotenv()
+(defun jong-dotenv-load-dotenv-test ()
   (interactive)
+  (let (file-path)
+	(setq file-path (format "%s/.env" (getenv "HOME")))
+	(jong-dotenv-load-dotenv file-path)
+	)
   )
+
+
+(defun jong-dotenv-load-dotenv (file-path)
+  (let (lines)
+	(with-current-buffer (find-file-noselect file-path)
+	  (setq lines (split-string
+				   (save-restriction
+					 (widen)
+					 (buffer-substring-no-properties
+					  (point-min)
+					  (point-max)))
+				   "\n" t))
+	  )
+	(print lines)
+	)
+  )
+
+
+(provide 'dotenv-mode)
