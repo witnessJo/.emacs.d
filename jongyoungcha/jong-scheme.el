61f82e247c86ef61c8d0ff768350bd1c6932a6ff
Richard Jo <richard@sentbe.com>
2021-01-02 22:31:08 +0900

    remove (pakcage-initialize)

--- jongyoungcha/jong-scheme.el
+++ jongyoungcha/jong-scheme.el
@@ -1,16 +1,7 @@
 (gnutls-available-p)
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/")
-			 '("marmalade" . "https://marmalade-repo.org/packages/"))
 
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-
-(package-initialize)
-	
 (use-package xscheme :ensure t)
 
 (add-hook 'scheme-mode-hook
