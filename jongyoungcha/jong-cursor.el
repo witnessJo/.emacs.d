7aa8ac17daa9c778dd0657bfeb400c8e9f73dcb6
Richard Jo <richard@sentbe.com>
2021-06-05 21:01:49 +0900

    modified key bindings

--- jongyoungcha/jong-cursor.el
+++ jongyoungcha/jong-cursor.el
@@ -1,17 +1,32 @@
 ;;; Code
 
 
-(set (make-local-variable 'jong-beginning-of-line-text-pos) (point))
-(defun jong-beginning-of-line-text()
+(set (make-local-variable 'jong-edit-beginning-of-line-text-pos) (point))
+
+(defun jong-edit-beginning-of-line-text()
   "Beginning of line."
   (interactive)
   (let ()
-	(if (equal jong-beginning-of-line-text-pos (point))
+	(if (equal jong-edit-beginning-of-line-text-pos (point))
 		(beginning-of-line)
 	  (beginning-of-line-text))
-	(setq jong-beginning-of-line-text-pos (point))
+	(setq jong-edit-beginning-of-line-text-pos (point))
 	)
   )
+
+
+(defun jong-edit-paste-text()
+  (interactive)
+  (let ()
+	(if (use-region-p)
+		(progn
+		  (call-interactively 'delete-backward-char)
+		  (call-interactively 'yank))
+	  (call-interactively 'yank))
+	)
+  )
+
+
 
 
 
