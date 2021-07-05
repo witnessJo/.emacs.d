961756ef10e09fb4a4d00c9d097ea1802eaf9a6b
Richard Jo <richard@sentbe.com>
2021-07-05 20:29:56 +0900

    modified...

---
+++ jongyoungcha/jong-sql.el
@@ -0,0 +1,18 @@
+;;; Code
+
+(setq lsp-sqls-connections
+      '(((driver . "mysql") (dataSourceName . "yyoncho:local@tcp(localhost:3306)/foo"))
+		((driver . "postgresql") (dataSourceName . "host=127.0.0.1 port=5432 user=postgres password=sentbe1234. dbname=chandra sslmode=disable"))
+	    ((driver . "postgresql") (dataSourceName . "host=127.0.0.1 port=5432 user=postgrsql password=local dbname=chandra sslmode=disable"))
+		))
+
+(defun jong-sql-install-sqls
+	(interactive)
+  )
+
+(add-hook 'sql-mode-hook (lambda()
+						   (lsp)
+						   (local-set-key (kbd "C-<return>") 'lsp-sql-execute-paragraph)
+						   ))
+
+(provide 'jong-sql)
