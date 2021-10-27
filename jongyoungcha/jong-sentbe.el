a955de4205a56d0cf4f422f4d92f1c9dafdf8197
Richard Jo <richard@sentbe.com>
2021-10-27 21:47:11 +0900

    modified

---
+++ jongyoungcha/jong-sentbe.el
@@ -0,0 +1,29 @@
+;;; Code:
+;;; message
+
+
+(defcustom jong-sentbe-helm-repo nil
+  "Just path value of sentbe helm repo."
+  :type 'string
+  :group 'jong-sentbe)
+
+
+(defun jong-sentbe-copy-file-to-helm-demeter-pb()
+  (interactive)
+  (let ((src-path)
+		(dst-path))
+	(print "jong-sentbe-copy-file-to-helm-demeter-pb")
+	(setq src-path (jong-sentbe-select-file))
+	(setq dst-path (jong-sentbe-select-file))
+	(print "src-path:")
+	(print "dst-path:")
+	)
+  )
+
+(defun jong-sentbe-select-file()
+  (interactive)
+  )
+
+
+
+(provide jong-sentbe)
